7a64d294695180b2fae02e3c523737a88933929d
Fadilath <adegnilefadilath@gmail.com>
2026-01-19 11:49:31 +0100

    Front-end : Ajout de la vue de modification et icônes d'édition

--- resources/views/schedule/index.blade.php
+++ resources/views/schedule/index.blade.php
@@ -9,4 +9,14 @@
         <table class="w-full">
             </table>
     </div>
+    <div class="bg-blue-100 p-3 rounded-lg border-l-4 border-blue-500 relative">
+    <p class="font-bold text-blue-800">Algorithmique</p>
+    <p class="text-xs text-blue-600">Salle 102 - M. Dossou</p>
+    
+    <a href="/mon-planning/modifier" class="absolute top-1 right-1 text-blue-400 hover:text-blue-600">
+        <svg xmlns="http://www.w3.org/2000/svg" class="h-4 w-4" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+            <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M15.232 5.232l3.536 3.536m-2.036-5.036a2.5 2.5 0 113.536 3.536L6.5 21.036H3v-3.572L16.732 3.732z" />
+        </svg>
+    </a>
+</div>
 @endsection
--- routes/web.php
+++ routes/web.php
@@ -13,3 +13,7 @@ Route::get('/mon-planning', function () {
 Route::get('/mon-planning/ajouter', function () {
     return view('schedule.create');
 });
+
+Route::get('/mon-planning/modifier', function () {
+    return view('schedule.edit');
+});
